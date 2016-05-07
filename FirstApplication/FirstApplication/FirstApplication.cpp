141ffd7e8b134d9f92d7054094e953709cf36a53
Toxic.dev <toxic.dev@frackstudios.com>
2016-05-07 05:41:41 -0400

    first application.

---
+++ FirstApplication/FirstApplication/FirstApplication.cpp
@@ -0,0 +1,12 @@
+// FirstApplication.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+int main()
+{
+	std::cout << "Hello World Application!\n";
+    return 0;
+}
+
