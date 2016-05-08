5ff76e7a29abc628ddf62c24b1102e9947aac62c
Toxic.dev <toxic.dev@frackstudios.com>
2016-05-07 20:34:51 -0400

    rewrote the class to look better and have encapsulation.

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -3,28 +3,37 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
+
 int main() 
 {
-	// introduce the game
+	PrintIntro();
+	GetGuessAndPrintBack();
+	cout << endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of.\n";
 	cout << endl;
+	return;
+}
 
-	// get a guess from the player
+// get a guess from the player
+string GetGuessAndPrintBack()
+{
 	string Guess = "";
-	cout << "Please enter your guess for the isogram: "; 
-	getline(cin, Guess);
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	// get a guess from the player
 	cout << "Please enter your guess for the isogram: ";
 	getline(cin, Guess);
 
 	// repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
-	cout << endl;
-	return 0;
+	return Guess;
 }
